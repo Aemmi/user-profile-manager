4dddd1b6235d589524778ea067e8f7740b94ae1e
Emmanuel Ayeh <Emmanuel.Ayeh@abujaelectricity.com>
2024-11-01 13:17:30 +0000

    update components and welcome.blade.php

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,1 +1,7 @@
 import './bootstrap';
+import { createApp } from 'vue';
+import HomeComponent from './components/HomeComponent.vue';
+
+const app = createApp({});
+app.component('home-component', HomeComponent); // Changed to kebab-case
+app.mount('#app');
---
+++ resources/js/components/HomeComponent.vue
@@ -0,0 +1,17 @@
+<template>
+    <div>
+        <h1 style="color: red;">Hello from Laravel/VueJs!</h1>
+    </div>
+</template>
+
+<script>
+    export default{
+        name: 'HomeComponent',
+    }
+</script>
+
+<style scoped>
+    h1{
+        color: #42b983;
+    }
+</style>
